7b468e03ff9539b4e755e7f61ea82ec104db23ad
MauricioMSoares <mauricio.soares@sptech.school>
2024-03-29 19:59:56 -0300

    feat: added permission settings

---
+++ main/users/permissions.py
@@ -0,0 +1,18 @@
+from rest_framework import permissions
+
+class IsUserOwnerOrGetAndPostOnly(permissions.BasePermission):
+    """
+    Custom permissions for the UserViewSet to only allow users to edit their own profile. Otherwise, Get and Post only.
+    """
+
+    def has_permission(self, request, view):
+        return True
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        if not request.user.is_anonymous:
+            return request.user == obj
+
+        return False
