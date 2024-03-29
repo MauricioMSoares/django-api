a4de0b5535d218ab51609c42f06fc6a53fac06ca
MauricioMSoares <mauricio.soares@sptech.school>
2024-03-29 17:50:08 -0300

    feat: added router to the project

--- main/main/urls.py
+++ main/main/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from users import router as users_api_router
+
+api_url_patterns = [
+    path(r"accounts/", include(users_api_router.router.urls))
+]
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api/", include(api_url_patterns))
 ]
