0f2fc8ef163b4d988d3ea9ab305f1d93be52b87f
MauricioMSoares <mauricio.soares@sptech.school>
2024-03-29 20:38:57 -0300

    feat: added and configured django oauth2 libs

--- README.md
+++ README.md
@@ -39,3 +39,32 @@ pip install markdown
 ```
 
 Make sure to add rest_framework on settings.py, INSTALLED_APPS.
+
+### Django OAuth2 dependency installing
+
+```
+pip install django-rest-framework-social-oauth2
+cd project_name
+python manage.py makemigrations
+python manage.py migrate
+```
+
+Make sure to add oauth2_provider, social_django and rest_framework_social_oauth2 on settings.py, INSTALLED_APPS. <br>
+Also, make sure to add social_django.context_processors.backends and social_django.context_processors.login_redirect on settings.py, TEMPLATES. <br>
+Next, add the following line inside your urls.py
+
+```
+auth_api_urls = [
+    path(r"", include("rest_framework_social_oauth2.urls")),
+]
+```
+
+Then, define oauth2_provider.contrib.rest_framework.OAuth2Authentication and rest_framework_social_oauth2.authentication.SocialAuthentication on settings.py, REST_FRAMEWORK. <br>
+Lastly, set the following dictionary on settings.py:
+
+```
+AUTHENTICATION_BACKENDS = {
+    'django.contrib.auth.backends.ModelBackend',
+    'rest_framework_social_oauth2.backends.DjangoOAuth2'
+}
+```
--- main/main/settings.py
+++ main/main/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'oauth2_provider',
+    'social_django',
+    'rest_framework_social_oauth2',
     'users'
 ]
 
@@ -64,6 +67,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
@@ -107,9 +112,20 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        #Rest Framework Authentication Classes
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+
+        #OAuth2 Authentication Classes
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
+        'rest_framework_social_oauth2.authentication.SocialAuthentication',
     ]
+}
+
+
+AUTHENTICATION_BACKENDS = {
+    'django.contrib.auth.backends.ModelBackend',
+    'rest_framework_social_oauth2.backends.DjangoOAuth2'
 }
 
 
