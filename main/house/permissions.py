197a62e0e5201aeea62d6f66e08b4c29077fe198
MauricioMSoares <mauricio.soares@sptech.school>
2024-03-30 16:01:33 -0300

    feat: added house serializers, viewsets and permissions

---
+++ main/house/permissions.py
@@ -0,0 +1,22 @@
+from rest_framework import permissions
+
+
+class IsHouseManagerOrNone(permissions.BasePermission):
+    """
+    Custom permissions for the HouseViewSet to only allow specific privileges for editing specific house attributes.
+    """
+
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        if not request.user.is_anonymous:
+            return True
+
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return request.user.profile == obj.manager
