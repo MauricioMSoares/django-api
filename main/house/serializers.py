8767885b5d0d61e766769e8b103f74d3551a2d36
MauricioMSoares <mauricio.soares@sptech.school>
2024-03-30 17:07:34 -0300

    feat: added feature to join and leave houses as a member

--- main/house/serializers.py
+++ main/house/serializers.py
@@ -4,6 +4,12 @@ from .models import House
 
 class HouseSerializer(serializers.ModelSerializer):
     members_count = serializers.IntegerField(read_only=True)
+    members = serializers.HyperlinkedRelatedField(
+        read_only=True, many=True, view_name="profile-detail"
+    )
+    manager = serializers.HyperlinkedRelatedField(
+        read_only=True, many=False, view_name="profile-detail"
+    )
 
     class Meta:
         model = House
@@ -16,6 +22,7 @@ class HouseSerializer(serializers.ModelSerializer):
             "manager",
             "description",
             "members_count",
+            "members",
             "points",
             "completed_tasks_count",
             "notcompleted_tasks_count",
--- main/house/viewsets.py
+++ main/house/viewsets.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import House
 from .serializers import HouseSerializer
 from .permissions import IsHouseManagerOrNone
@@ -10,3 +12,42 @@ class HouseViewSet(viewsets.ModelViewSet):
         IsHouseManagerOrNone,
     ]
     serializer_class = HouseSerializer
+
+    @action(detail=True, methods=["post"], name="Join", permission_classes=[])
+    def join(self, request, pk=None):
+        try:
+            house = self.get_object()
+            user_profile = request.user.profile
+            if user_profile.house == None:
+                user_profile.house = house
+                user_profile.save()
+                return Response(status=status.HTTP_204_NO_CONTENT)
+            elif user_profile in house.members.all():
+                return Response(
+                    {"detail": "Already a member in this house."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            else:
+                return Response(
+                    {"detail": "Already a member in another house."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+        except Exception as e:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    @action(detail=True, methods=["post"], name="Leave", permission_classes=[])
+    def leave(self, request, pk=None):
+        try:
+            house = self.get_object()
+            user_profile = request.user_profile
+            if user_profile in house.members.all():
+                user_profile.house = None
+                user_profile.save()
+                return Response(status=status.HTTP_204_NO_CONTENT)
+            else:
+                return Response(
+                    message={"detail", "Not a member in this house."},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+        except Exception as e:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
