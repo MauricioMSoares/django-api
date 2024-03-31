4e28ba63c113b72639305c00401ebda40e836753
MauricioMSoares <mauricio.soares@sptech.school>
2024-03-31 01:39:21 -0300

    feat: updated task viewsets and permissions

--- main/task/permissions.py
+++ main/task/permissions.py
@@ -17,3 +17,33 @@ class IsAllowedToEditTaskListElseNone(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return request.user.profile == obj.created_by
+
+
+class IsAllowedToEditTaskElseNone(permissions.BasePermission):
+    """
+    Custom permissions for TaskViewSet to only allow the creator editing permissions.
+    """
+
+    def has_permission(self, request, view):
+        if not request.user.is_anonymous:
+            return request.user.profile.house != None
+        
+        return False
+    
+    def has_object_permission(self, request, view, obj):
+        return request.user.profile.house == obj.task_list.house
+
+
+class IsAllowedToEditAttachmentElseNone(permissions.BasePermission):
+    """
+    Custom permissions for AttachmentViewSet to only allow the creator editing permissions.
+    """
+
+    def has_permission(self, request, view):
+        if not request.user.is_anonymous:
+            return request.user.profile.house != None
+        
+        return False
+    
+    def has_object_permission(self, request, view, obj):
+        return request.user.profile.house == obj.task.task_list.house
