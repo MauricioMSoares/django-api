04e777e11e9dc08fafea3feeb372ee250f2a5d03
MauricioMSoares <mauricio.soares@sptech.school>
2024-03-31 01:15:53 -0300

    feat: added task viewsets, serializers and permissions

---
+++ main/task/permissions.py
@@ -0,0 +1,19 @@
+from rest_framework import permissions
+
+
+class IsAllowedToEditTaskListElseNone(permissions.BasePermission):
+    """
+    Custom permissions for TaskListViewSet to only allow the creator editing permissions.
+    """
+
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        if not request.user.is_anonymous:
+            return True
+        
+        return False
+
+    def has_object_permission(self, request, view, obj):
+        return request.user.profile == obj.created_by
